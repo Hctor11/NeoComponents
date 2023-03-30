c498977569066cbb5f353a2a2b2288478e029ec8
hector <hectorrrivera1010@gmail.com>
2023-03-29 22:50:38 -0600

    inital commit

--- NeoComponents/PanelRadius.cs
+++ NeoComponents/PanelRadius.cs
@@ -16,9 +16,20 @@ namespace NeoComponents
         private Color gradientTopColor = Color.DodgerBlue;
         private Color gradientBottomColor = Color.CadetBlue;
 
-        // constructor
+        // Constructor
         public PanelRadius() {
             this.BackColor = Color.White;
+            this.ForeColor = Color.Black;
+            this.Size = new Size(450, 300);
         }
+
+        // Propiedades
+        public int BorderRadius { get => borderRadius; set => borderRadius=value; }
+        public int GradientAngle { get => gradientAngle; set => gradientAngle=value; }
+        public Color GradientTopColor { get => gradientTopColor; set => gradientTopColor=value; }
+        public Color GradientBottomColor { get => gradientBottomColor; set => gradientBottomColor=value; }
+
+
+
     }
 }
