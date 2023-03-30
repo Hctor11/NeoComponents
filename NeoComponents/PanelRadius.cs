7324855271541770d006a30a6dad774433ff656a
hector <hectorrrivera1010@gmail.com>
2023-03-29 22:45:46 -0600

    Agregar archivos de proyecto.

---
+++ NeoComponents/PanelRadius.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace NeoComponents
+{
+    public class PanelRadius : Panel
+    {
+        // Campos
+        private int borderRadius = 30;
+        private int gradientAngle = 90;
+        private Color gradientTopColor = Color.DodgerBlue;
+        private Color gradientBottomColor = Color.CadetBlue;
+
+        // constructor
+        public PanelRadius() {
+            this.BackColor = Color.White;
+        }
+    }
+}
