c8945080b452a39991eec08a1533b71820ee5bf1
hector <hectorrrivera1010@gmail.com>
2023-03-30 23:09:36 -0600

    created properties getters and setters

--- NeoComponents/NeoTextBox.cs
+++ NeoComponents/NeoTextBox.cs
@@ -12,7 +12,7 @@ namespace NeoComponents
     public class NeoTextBox : Control
     {
         // Campos
-        private int bordeSize = 0;
+        private int borderSize = 0;
         private int radius = 50;
         private Color borderColor = Color.Transparent;
         private Color backColor;
@@ -36,8 +36,109 @@ namespace NeoComponents
             textBox.BorderStyle = BorderStyle.None;
             textBox.BackColor = backColor;
             BackColor = Color.Transparent;
+            backColor = Color.White;
+            Font = new Font("Century Gothic", 12F);
+            DoubleBuffered = true;
+        }
 
+        // Propiedades
+        public char PasswordChar
+        {
+            get { return textBox.PasswordChar; }
+            set 
+            { 
+                textBox.PasswordChar = value; 
+                base.Invalidate();
+            }
+        }
 
+        public bool UseSystemPasswordChar
+        {
+            get { return textBox.UseSystemPasswordChar; }
+            set
+            {
+                textBox.UseSystemPasswordChar = value;
+                base.Invalidate();
+            }
+        }
+
+        public Color NeoBackColor
+        {
+            get { return backColor; }
+            set
+            {
+                backColor = value;
+                if (backColor != Color.Transparent) 
+                {
+                    textBox.BackColor = backColor;
+                    base.Invalidate();
+                }
+            }
+        }
+
+        public override Color BackColor 
+        { 
+            get => base.BackColor ; 
+            set => base.BackColor= Color.Transparent; 
+        }
+
+        public Color BorderColor
+        {
+            get => borderColor;
+            set 
+            { 
+                borderColor=value;
+                Invalidate();
+            }
+        }
+
+        public Color PlaceholderColor
+        {
+            get => placeholderColor;
+            set
+            {
+                placeholderColor=value;
+                Invalidate();
+            }
+        }
+
+        public int Radius
+        {
+            get => radius;
+            set 
+            {
+                radius=value;
+                Invalidate();
+            }
+        }
+
+        public int BorderSize
+        {
+            get => borderSize;
+            set
+            {
+                borderSize=value;
+                Invalidate();
+            }
+        }
+
+        public string PlaceHolder
+        { 
+            get => placeholder;
+            set
+            {
+                placeholder=value;
+                if (Text == placeholder || Text == string.Empty)
+                {
+                    Text = placeholder;
+                    textBox.ForeColor = placeholderColor;
+                }
+                else
+                {
+                    textBox.ForeColor = mColor;
+                }
+                Invalidate();
+            }
         }
     }
 }
