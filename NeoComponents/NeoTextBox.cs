3bfcb99a0696bf9b14500861b42b28b329fd7bd7
hector <hectorrrivera1010@gmail.com>
2023-03-30 22:42:38 -0600

    inital commit for NeoTextBox

---
+++ NeoComponents/NeoTextBox.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NeoComponents
+{
+    public class NeoTextBox : Control
+    {
+        // Campos
+        private int bordeSize = 0;
+        private int radius = 50;
+        private Color borderColor = Color.Transparent;
+        private Color backColor;
+        private Color placeholderColor = Color.Gray;
+        private Color mColor = Color.Transparent;
+        private string placeholder;
+        private GraphicsPath shape;
+        private GraphicsPath innerRectangle;
+        public TextBox textBox = new TextBox();
+
+        // Constructor
+        public NeoTextBox()
+        {
+            base.SetStyle(ControlStyles.SupportsTransparentBackColor, true);
+            base.SetStyle(ControlStyles.UserPaint, true);
+            base.SetStyle(ControlStyles.ResizeRedraw, true);
+
+            base.Controls.Add(textBox);
+            base.Size = new Size(200, 50);
+            textBox.Parent = this;
+            textBox.BorderStyle = BorderStyle.None;
+            textBox.BackColor = backColor;
+            BackColor = Color.Transparent;
+
+        }
+    }
+}
---
+++ TestBorderRadius/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace TestBorderRadius
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panelRadius1 = new NeoComponents.PanelRadius();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // panelRadius1
+            // 
+            this.panelRadius1.BackColor = System.Drawing.Color.White;
+            this.panelRadius1.BorderRadius = 30;
+            this.panelRadius1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panelRadius1.ForeColor = System.Drawing.Color.Black;
+            this.panelRadius1.GradientAngle = 90;
+            this.panelRadius1.GradientBottomColor = System.Drawing.Color.PaleTurquoise;
+            this.panelRadius1.GradientTopColor = System.Drawing.Color.Navy;
+            this.panelRadius1.Location = new System.Drawing.Point(488, 111);
+            this.panelRadius1.Name = "panelRadius1";
+            this.panelRadius1.Size = new System.Drawing.Size(252, 300);
+            this.panelRadius1.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox1.Location = new System.Drawing.Point(121, 272);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(246, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.panelRadius1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private NeoComponents.PanelRadius panelRadius1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
