37230eac4efa73d8d7cb758e8c72053bbf380e4c
hector <hectorrrivera1010@gmail.com>
2023-04-02 20:41:40 -0600

    created NeoButtons class

---
+++ NeoComponents/NeoButton.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NeoComponents
+{
+    public class NeoButton : Button
+    {
+        // atributos
+        private int borderSize = 0;
+        private int borderRadius = 50;
+        private Color borderColor = Color.DodgerBlue;
+
+        // Propiedades
+        public int BorderSize
+        {
+            get => borderSize;
+            set 
+            { 
+                borderSize = value;
+                Invalidate();
+            }
+        }
+
+        public int BorderRadius
+        {
+            get => borderRadius;
+            set
+            {
+                borderRadius = value;
+                Invalidate();
+            }
+        }
+
+        public Color BackgroundColor
+        {
+            get => BackColor;
+            set => BackColor = value;
+
+        }
+
+        public Color TextColor
+        {
+            get => ForeColor;
+            set => ForeColor = value;
+
+        }
+
+
+    }
+}
